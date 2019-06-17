a3ec78eb218f31aa477167841e0f5da2f8760e31
lamaustin78 <46605068+lamaustin78@users.noreply.github.com>
2019-06-17 16:14:53 -0400

    for loops

---
+++ forloops.c
@@ -0,0 +1,29 @@
+int main(void){}
+	int i; 
+	i = 0;
+	for (i = 0; i < 5; i = i + 1)
+	{
+		
+	}
+}
+
+int i;
+i = 0;
+for (i = 10; i > 0; i = i - 1)
+{
+	
+}
+
+int i;
+i = 0;
+for (i = 100; i > 1; i = i / 2)
+{
+	
+}
+
+int i;
+for (i = 0; i < 10; i = i + 1)
+{
+	i = i + 1;
+}
+
